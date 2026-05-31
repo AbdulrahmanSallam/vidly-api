089a6994709a55837f7bb00149164924f347b1ce
Abdulrahman Sallam <abdulrahman.sallam.official@gmail.com>
2026-05-31 06:08:14 +0300

    Add debugger and helmet

--- vidly.api/index.js
+++ vidly.api/index.js
@@ -1,12 +1,15 @@
+const helmet = require("helmet");
+const appDebugger = require("debug")("app");
 const Joi = require("joi");
 const genres = require("./routes/genres");
 const express = require("express");
 const app = express();
 
+app.use(helmet);
 app.use(express.json());
 app.use("/api/genres", genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`listen to ${port}...`);
+  appDebugger(`listen to ${port}...`);
 });
